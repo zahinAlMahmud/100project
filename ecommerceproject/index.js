a43c1a992eafc3b24d83b01239c0cbc01fe12a37
zahinAlMahmud <zahinmahmud327@gmail.com>
2026-05-14 00:17:32 +0600

    css and js update

--- ecommerceproject/index.js
+++ ecommerceproject/index.js
@@ -1,23 +1,38 @@
-let  add =document.getElementById('add');
-let  wishlist =document.getElementById('wishlist');
+let add = document.getElementById('add');
+let wishlist = document.getElementById('wishlist');
 let count = 0;
 let cart = document.getElementById('cart');
 let list = document.getElementById('list');
 
 // add.addEventListener('click',()=>{++count
 //     console.log(count)
+
 // })
 
-add.addEventListener('click',(e)=>{
+function updatedButton() {
+    if (count >= 10) {
+        add.disabled = true;
+        wishlist.disabled = true;
+        console.log('button disable')
+    } else {
+        add.disabled = false;
+        wishlist.disabled = false;
+        console.log('button not disable')
+    }
+}
+
+add.addEventListener('click', (e) => {
     e.preventDefault();
     count = count + 1;
-    console.log(count); 
-    cart.innerText =count;
+    console.log(count);
+    cart.innerText = count;
+    updatedButton();
 })
 
-wishlist.addEventListener('click',(e)=>{
+wishlist.addEventListener('click', (e) => {
     e.preventDefault();
     count = count + 1;
-    console.log(count); 
-    list.innerText =count;
+    console.log(count);
+    list.innerText = count;
+    updatedButton();
 })
